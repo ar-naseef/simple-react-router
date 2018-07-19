9b53f1310f85412e178a9a99046ebd5cebddc375
naseefxyz <naseef@appler.xyz>
2018-07-19 20:49:25 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import loginComp from './Components/login.js';
+import homeComp from './Components/home.js';
+import another from './Components/another.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={homeComp} />
+          <Route exact path="/login" component={loginComp} />
+          <Route path="/login/another" component={another} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/Components/another.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class another extends Component {
+  render() {
+    return (
+      <div>
+          Another
+      </div>
+    );
+  }
+}
+
+export default another;
